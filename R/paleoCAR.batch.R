4058b130bca16341e7c2198a6fe11359e1ed1fff
Bocinsky <bocinsky@gmail.com>
2015-10-07 10:10:13 -0700

    fixed NA bug in reconstruction step

--- R/paleoCAR.batch.R
+++ R/paleoCAR.batch.R
@@ -28,46 +28,51 @@
 #'   \item{\code{errors}  The PaleoCAR reconstruction average LOOCV error, as computed by \code{\link{errors.paleocar.models.batch}}.}
 #'   \item{\code{sizes}  The PaleoCAR model sizes, as computed by \code{\link{size.paleocar.models.batch}}.}
 #' }
-paleoCAR.batch <- function(chronologies, predictands, calibration.years, prediction.years=NULL, label, out.dir="./OUTPUT/", min.width=NULL, meanVar = "none", floor=NULL, ceiling=NULL, asInt=F, force.redo=F, verbose=F, return.objects=T){
+paleoCAR.batch <- function(chronologies, predictands, calibration.years, prediction.years=NULL, label, out.dir="./OUTPUT/", min.width=NULL, meanVar = "none", floor=NULL, ceiling=NULL, asInt=F, force.redo=F, verbose=F, generate.reconstruction=T, return.objects=T){
   t <- Sys.time()
   if(verbose) cat("\nCalculating all models")
   models <- paleoCAR.models.batch(chronologies=chronologies, predictands=predictands, calibration.years=calibration.years, prediction.years=prediction.years, label=label, out.dir=out.dir, min.width=min.width, force.redo=force.redo, verbose=verbose)
   
-  if(verbose) cat("\nGenerating reconstruction")
-  if(!force.redo & file.exists(paste(out.dir,label,".recon.tif",sep=''))){
-    
-    recon <- raster::brick(paste(out.dir,label,".recon.tif",sep=''))
-    
-  }else{
-    recon <- predict.paleocar.models.batch(models=models, meanVar=meanVar, prediction.years=prediction.years)
-    
-    if(!is.null(floor)){
-      recon <- raster::calc(recon,function(x){x[x<floor] <- floor; return(x)})
+  if(generate.reconstruction){
+    if(verbose) cat("\nGenerating reconstruction")
+    if(!force.redo & file.exists(paste(out.dir,label,".recon.tif",sep=''))){
+      
+      recon <- raster::brick(paste(out.dir,label,".recon.tif",sep=''))
+      
+    }else{
+      recon <- predict.paleocar.models.batch(models=models, meanVar=meanVar, prediction.years=prediction.years)
+      
+      if(!is.null(floor)){
+        recon <- raster::calc(recon,function(x){x[x<floor] <- floor; return(x)})
+      }
+      if(!is.null(ceiling)){
+        recon <- raster::calc(recon,function(x){x[x>ceiling] <- ceiling; return(x)})
+      }
+      if(asInt){
+        recon <- raster::calc(recon,function(x){round(x,digits=0)})
+        type <- "INT2S"
+      }else{type="FLT4S"}
+      raster::writeRaster(recon,paste(out.dir,label,".recon.tif",sep=''), datatype=type, options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
+      
+      #     if(asInt){
+      #       recon$errors <- calc(recon$errors,function(x){round(x,digits=0)})
+      #       type="INT2U"
+      #     }else{type="FLT4S"}
+      #     raster::writeRaster(recon$errors,paste(out.dir,label,".errors.tif",sep=''), datatype=type, options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
+      #     
+      #     raster::writeRaster(recon$sizes,paste(out.dir,label,".size.tif",sep=''), datatype="INT2U", options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
+      #     
+      # saveRDS(recon,paste(out.dir,label,".recon.Rds",sep=''),compress='xz')
     }
-    if(!is.null(ceiling)){
-      recon <- raster::calc(recon,function(x){x[x>ceiling] <- ceiling; return(x)})
-    }
-    if(asInt){
-      recon <- raster::calc(recon,function(x){round(x,digits=0)})
-      type <- "INT2S"
-    }else{type="FLT4S"}
-    raster::writeRaster(recon,paste(out.dir,label,".recon.tif",sep=''), datatype=type, options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
-    
-    #     if(asInt){
-    #       recon$errors <- calc(recon$errors,function(x){round(x,digits=0)})
-    #       type="INT2U"
-    #     }else{type="FLT4S"}
-    #     raster::writeRaster(recon$errors,paste(out.dir,label,".errors.tif",sep=''), datatype=type, options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
-    #     
-    #     raster::writeRaster(recon$sizes,paste(out.dir,label,".size.tif",sep=''), datatype="INT2U", options=c("COMPRESS=DEFLATE", "ZLEVEL=9", "INTERLEAVE=BAND"), overwrite=T, setStatistics=FALSE)
-    #     
-    # saveRDS(recon,paste(out.dir,label,".recon.Rds",sep=''),compress='xz')
   }
-  
   if(verbose) cat("\nThe entire reconstruction took", difftime(Sys.time(),t,units='mins'),"minutes")
   
   if(return.objects){
-    return(list(models=models,recon=recon)) 
+    if(generate.reconstruction){
+      return(list(models=models,recon=recon)) 
+    }else{
+      return(models)
+    }
   }else{
     rm(models,recon); gc(); gc()
     return(NULL)
